9cc42f2e4de9a7565560a37dad7b0f3b445eee4a
Paul-Eloi Dalmas <pauleloidalmas@gmail.com>
2023-03-14 12:07:32 +0100

    Highlight and Info
    can show info on any country clicked if part of the list of countries.
    Cannot highlight countries dynamically : maybe arraylist + name property on each layer ?

--- app/src/main/java/com/example/mapstest/MapsActivity.kt
+++ app/src/main/java/com/example/mapstest/MapsActivity.kt
@@ -1,22 +1,28 @@
 package com.example.mapstest
 
 
+
+import android.content.ContentValues.TAG
 import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.google.maps.android.data.geojson.GeoJsonLayer
+import com.google.maps.android.data.geojson.GeoJsonMultiPolygon
+import com.google.maps.android.data.geojson.GeoJsonPolygon
 import org.json.JSONException
 import java.io.IOException
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class MapsActivity : FragmentActivity(), OnMapReadyCallback {
     private var mMap: GoogleMap? = null
@@ -30,33 +36,87 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
 
     }
+
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap!!.uiSettings.isZoomControlsEnabled = true
 
-        val clickedCountryLatLng = LatLng(40.4167754, -3.7037902)
-        val informationWindow = mMap?.addMarker(
-            MarkerOptions()
-                .position(clickedCountryLatLng)
-                .title("Madrid")
-                .snippet("Information")
-        )
-        informationWindow?.showInfoWindow()
 
-        val madrid = LatLng(40.416775, -3.70379)
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(madrid, 3f))
-        try {
-            val layer = GeoJsonLayer(mMap, R.raw.es_geojson, applicationContext)
+        val success = googleMap.setMapStyle(
+            MapStyleOptions.loadRawResourceStyle(
+                this, R.raw.style_json
+            )
+        )
+        val capitalCity = LatLng(40.416775, -3.70379)
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(capitalCity, 3f))
+        var newLayer : GeoJsonLayer = GeoJsonLayer(mMap, R.raw.us_geojson, applicationContext)
+            try {
+            val layer = GeoJsonLayer(mMap, R.raw.us_geojson, applicationContext)
             val style = layer.defaultPolygonStyle
-            style.fillColor = Color.MAGENTA
-            style.strokeColor = Color.MAGENTA
+            style.fillColor = Color.BLUE
+            style.strokeColor = Color.BLUE
             style.strokeWidth = 1f
+            style.isClickable = false
             layer.addLayerToMap()
+                newLayer = layer
         } catch (ex: IOException) {
             Log.e("IOException", ex.localizedMessage)
         } catch (ex: JSONException) {
             Log.e("JSONException", ex.localizedMessage)
         }
+        val markerList = mutableListOf<Marker>()
+        newLayer.map = null
+        mMap!!.setOnMapClickListener{
+
+
+            //Here, clear list of markers on click
+            markerList.forEach { it.remove() }
+            markerList.clear()
+
+
+            //Here, get country from click location
+            val geocoder = Geocoder(this, Locale.getDefault())
+            val addresses: List<Address> = geocoder.getFromLocation(it.latitude, it.longitude, 1) as List<Address>
+            var country: String = ""
+            if (addresses.isNotEmpty()) {
+                country = addresses[0].getCountryName()
+            }
+
+            //Here, check if country is part of displayed countries
+            val displayedCountries = ArrayList<String>()
+            displayedCountries.add("Spain")
+            displayedCountries.add("United States")
+
+
+            //Here, get Information based on country clicked
+
+            val displayedInformation : String = ""
+
+            //Here, create marker
+
+            if(displayedCountries.contains(country)){
+                newLayer.map = mMap
+                val clickedCountryLatLng = LatLng(it.latitude, it.longitude)
+                val informationWindow = mMap?.addMarker(
+                    MarkerOptions()
+                        .position(clickedCountryLatLng)
+                        .title(country.toString())
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_icon))
+                        .snippet(displayedInformation)//Will contain breed repartition in
+                )
+                informationWindow?.alpha = 0.0f;
+                informationWindow?.setInfoWindowAnchor(.5f, 1.0f);
+
+                if (informationWindow != null) {
+                    markerList.add(informationWindow)
+                }
+                informationWindow?.showInfoWindow()
+            } else { newLayer.map = null }
+
+        }
+
     }
 
 
