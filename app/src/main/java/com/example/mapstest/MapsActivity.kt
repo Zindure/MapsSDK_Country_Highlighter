784013b7e7a86e7a4b12934f3587f6a27140028a
Paul-Eloi Dalmas <pauleloidalmas@gmail.com>
2023-03-14 15:46:58 +0100

    Building Towards Dynamism
    Near dynamic json loading,
    Would directly lead to dynamic layers.
    The hard part would be to then dynamize event listener

--- app/src/main/java/com/example/mapstest/MapsActivity.kt
+++ app/src/main/java/com/example/mapstest/MapsActivity.kt
@@ -2,13 +2,17 @@ package com.example.mapstest
 
 
 
-import android.content.ContentValues.TAG
+import android.content.Context
+
 import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,12 +20,20 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.maps.android.data.geojson.GeoJsonLayer
-import com.google.maps.android.data.geojson.GeoJsonMultiPolygon
-import com.google.maps.android.data.geojson.GeoJsonPolygon
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.json.JSONException
+import org.json.JSONObject
+import java.io.File
+
 import java.io.IOException
+import java.io.InputStream
+import java.nio.file.*
+import java.nio.file.attribute.BasicFileAttributes
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.io.path.Path
 
 
 class MapsActivity : FragmentActivity(), OnMapReadyCallback {
@@ -39,7 +51,11 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
 
 
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @OptIn(ExperimentalSerializationApi::class)
     override fun onMapReady(googleMap: GoogleMap) {
+
         mMap = googleMap
         mMap!!.uiSettings.isZoomControlsEnabled = true
 
@@ -51,23 +67,63 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
         )
         val capitalCity = LatLng(40.416775, -3.70379)
         mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(capitalCity, 3f))
-        var newLayer : GeoJsonLayer = GeoJsonLayer(mMap, R.raw.us_geojson, applicationContext)
-            try {
-            val layer = GeoJsonLayer(mMap, R.raw.us_geojson, applicationContext)
-            val style = layer.defaultPolygonStyle
+
+        //var newLayer : GeoJsonLayer = GeoJsonLayer(mMap, R.raw.us_geojson, applicationContext)
+
+        var layerArray  = ArrayList<GeoJsonLayer>()
+        var countryArray = arrayOf("United States", "Spain")
+
+
+        // List of JSON files
+        val jsonFiles = listOf("es_geojson.json", "us_geojson.json")
+        // List of JSON objects
+        val jsonObjects = mutableListOf<JSONObject>()
+        // Loop through the list of files and import JSON objects from each file
+        for (jsonFile in jsonFiles) {
+            //System.out.print(File("assets/json/es_geojson.json"))
+
+            println("DIRECTORY" + System.getProperty("user.dir"))
+
+
+            val jsonString = File("app\\src\\androidTest\\assets\\json\\" + jsonFile).readText()
+            val jsonObject = Json.decodeFromString<JSONObject>(jsonString)
+            val tmpLayer = GeoJsonLayer(mMap, jsonObject)
+            val style = tmpLayer.defaultPolygonStyle
             style.fillColor = Color.BLUE
             style.strokeColor = Color.BLUE
             style.strokeWidth = 1f
             style.isClickable = false
-            layer.addLayerToMap()
-                newLayer = layer
-        } catch (ex: IOException) {
-            Log.e("IOException", ex.localizedMessage)
-        } catch (ex: JSONException) {
-            Log.e("JSONException", ex.localizedMessage)
+            layerArray.add(tmpLayer)
+            jsonObjects.add(jsonObject)
         }
+
+        for (layer in layerArray){
+            layer.addLayerToMap()
+        }
+
+        /*for (selectedCountry in countryArray)
+        {
+            try {
+                val layer = GeoJsonLayer(mMap, R.raw.us_geojson, applicationContext)
+                val style = layer.defaultPolygonStyle
+                style.fillColor = Color.BLUE
+                style.strokeColor = Color.BLUE
+                style.strokeWidth = 1f
+                style.isClickable = false
+                layer.addLayerToMap()
+                newLayer = layer
+            } catch (ex: IOException) {
+                Log.e("IOException", ex.localizedMessage)
+            } catch (ex: JSONException) {
+                Log.e("JSONException", ex.localizedMessage)
+            }
+        }*/
+
+
+
+
         val markerList = mutableListOf<Marker>()
-        newLayer.map = null
+        //newLayer.map = null
         mMap!!.setOnMapClickListener{
 
 
@@ -97,7 +153,7 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
             //Here, create marker
 
             if(displayedCountries.contains(country)){
-                newLayer.map = mMap
+                //newLayer.map = mMap
                 val clickedCountryLatLng = LatLng(it.latitude, it.longitude)
                 val informationWindow = mMap?.addMarker(
                     MarkerOptions()
@@ -113,7 +169,7 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
                     markerList.add(informationWindow)
                 }
                 informationWindow?.showInfoWindow()
-            } else { newLayer.map = null }
+            } else { //newLayer.map = null }
 
         }
 
@@ -121,4 +177,5 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
 
 
+    }
 }
