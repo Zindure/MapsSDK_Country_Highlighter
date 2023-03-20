0555b1e75c7ac8be1e3ee06dfdc441f691c2bb68
Paul-Eloi Dalmas <pauleloidalmas@gmail.com>
2023-03-20 13:51:16 +0100

    Highlighter complete
    Uses a CSV containing country name, geojson, and further custom data.
    Displays the first column of data on the country marker.

--- app/src/main/java/com/example/mapstest/MapsActivity.kt
+++ app/src/main/java/com/example/mapstest/MapsActivity.kt
@@ -11,9 +11,11 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.ImageAndVideo.toString
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.FragmentActivity
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -29,9 +31,11 @@ import java.io.File
 
 import java.io.IOException
 import java.io.InputStream
+import java.net.URI
 import java.nio.file.*
 import java.nio.file.attribute.BasicFileAttributes
 import java.util.*
+import java.util.Objects.toString
 import kotlin.collections.ArrayList
 import kotlin.io.path.Path
 
@@ -74,19 +78,26 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
         var countryArray = arrayOf("United States", "Spain")
 
 
-        // List of JSON files
-        val jsonFiles = listOf("es_geojson.json", "us_geojson.json")
+        // read from CSV File that contains country geoJSONS and names
+        val file = applicationContext.assets.open("data.csv")
+            .bufferedReader()
+            .use { it.readText()}
+        val countries = arrayListOf<List<String>>()
+        val rows: List<List<String>> = csvReader().readAll(file)
+        for (row in rows){
+            /*countries.add(listOf(row[0], row[4]))*/
+            countries.add(listOf(row[0], row[1], row[2]))
+        }
+
         // List of JSON objects
         val jsonObjects = mutableListOf<JSONObject>()
-        // Loop through the list of files and import JSON objects from each file
-        for (jsonFile in jsonFiles) {
-            //System.out.print(File("assets/json/es_geojson.json"))
-
-            println("DIRECTORY" + System.getProperty("user.dir"))
-
-
-            val jsonString = File("app\\src\\androidTest\\assets\\json\\" + jsonFile).readText()
-            val jsonObject = Json.decodeFromString<JSONObject>(jsonString)
+        val displayedCountries = ArrayList<String>()
+        // Loop through the countries and create associated layers
+        for (country in countries) {
+            val jsonString = country[0]
+            displayedCountries.add(country[1])
+            Log.d("GeoJSON : ", jsonString)
+            val jsonObject = JSONObject(jsonString)
             val tmpLayer = GeoJsonLayer(mMap, jsonObject)
             val style = tmpLayer.defaultPolygonStyle
             style.fillColor = Color.BLUE
@@ -97,73 +108,43 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
             jsonObjects.add(jsonObject)
         }
 
-        for (layer in layerArray){
-            layer.addLayerToMap()
-        }
-
-        /*for (selectedCountry in countryArray)
-        {
-            try {
-                val layer = GeoJsonLayer(mMap, R.raw.us_geojson, applicationContext)
-                val style = layer.defaultPolygonStyle
-                style.fillColor = Color.BLUE
-                style.strokeColor = Color.BLUE
-                style.strokeWidth = 1f
-                style.isClickable = false
-                layer.addLayerToMap()
-                newLayer = layer
-            } catch (ex: IOException) {
-                Log.e("IOException", ex.localizedMessage)
-            } catch (ex: JSONException) {
-                Log.e("JSONException", ex.localizedMessage)
-            }
-        }*/
-
-
-
-
+        //List of markers
         val markerList = mutableListOf<Marker>()
-        //newLayer.map = null
+
+        //Listens for map Click Events
         mMap!!.setOnMapClickListener{
 
-
-            //Here, clear list of markers on click
-            markerList.forEach { it.remove() }
-            markerList.clear()
-
+            //Here, clears the map
+            mMap!!.clear()
 
             //Here, get country from click location
             val geocoder = Geocoder(this, Locale.getDefault())
             val addresses: List<Address> = geocoder.getFromLocation(it.latitude, it.longitude, 1) as List<Address>
             var country: String = ""
-            if (addresses.isNotEmpty()) {
-                country = addresses[0].getCountryName()
+            if (addresses.isNotEmpty() && addresses[0].countryName != null) {
+                country = addresses[0].countryName
             }
 
-            //Here, check if country is part of displayed countries
-            val displayedCountries = ArrayList<String>()
-            displayedCountries.add("Spain")
-            displayedCountries.add("United States")
-
-
-            //Here, get Information based on country clicked
-
+            // Marker Information String
             val displayedInformation : String = ""
 
-            //Here, create marker
-
+            //Here, check if country is part of displayed countries
             if(displayedCountries.contains(country)){
-                //newLayer.map = mMap
+
                 val clickedCountryLatLng = LatLng(it.latitude, it.longitude)
                 val informationWindow = mMap?.addMarker(
                     MarkerOptions()
                         .position(clickedCountryLatLng)
                         .title(country.toString())
                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_icon))
-                        .snippet(displayedInformation)//Will contain breed repartition in
+                        .snippet(countries[displayedCountries.indexOf(country)][2])//Will contain breed repartition in
                 )
                 informationWindow?.alpha = 0.0f;
                 informationWindow?.setInfoWindowAnchor(.5f, 1.0f);
+
+                //Removes layer that has the same index as the country name from map and adds it again
+                layerArray[displayedCountries.indexOf(country)].removeLayerFromMap()
+                layerArray[displayedCountries.indexOf(country)].addLayerToMap()
 
                 if (informationWindow != null) {
                     markerList.add(informationWindow)
@@ -174,8 +155,6 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
         }
 
     }
-
-
 
     }
 }
